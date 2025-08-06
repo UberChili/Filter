2f149a7f4831a1dedd4a7b4db902f9d29ab5f335
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-08-05 22:18:58 -0600

    silly stuff, tomorrow real work starts

--- src/main.c
+++ src/main.c
@@ -5,17 +5,20 @@
 #include "filter.h"
 
 int main(int argc, char *argv[]) {
-    // Checking correct usage and getting input filename
+    // Checking correct usage
     if (argc != 2) {
         printf("Usage: ./filter filename.png\n");
         return 1;
     }
+    // Getting filename and output filename
     char *filename = get_filename(argv[1]);
     char *out_filename = get_out_filename(filename);
+    // TODO delete these prints, we won't need them later
     printf("filename: %s", filename);
     printf("out filename: %s", out_filename);
 
     // Freeing memory and exiting
     free(filename);
+    free(out_filename);
     return 0;
 }
